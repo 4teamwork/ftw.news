bf0b6b5ac3279d281ba5f0942721499d0a7e7566
Martin Bächtold <github@tph.ch>
2015-07-02 17:01:27 +0200

    Add Dublin core fields to news folder and news item.

--- ftw/news/contents/news_folder.py
+++ ftw/news/contents/news_folder.py
@@ -8,11 +8,6 @@ from zope.interface import implements
 
 
 class INewsFolderSchema(form.Schema):
-    title = schema.TextLine(
-        title=_(u'news_folder_title_label', default=u'Title'),
-        required=False,
-    )
-
     show_title = schema.Bool(
         title=_(u'news_folder_show_title_title_label', default=u'Show title'),
         default=True,

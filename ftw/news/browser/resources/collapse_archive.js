b563656b65366a4883eb4bf652a2b1cb3c5a076c
Kevin Bieri <k.bieri@4teamwork.ch>
2016-02-02 14:23:47 +0100

    Remove collapse animation of archive portlet.
    The customer don't want to wait until the content is visible.

--- ftw/news/browser/resources/collapse_archive.js
+++ ftw/news/browser/resources/collapse_archive.js
@@ -2,7 +2,6 @@ jQuery(function($){
     $(document).ready(function(){
         $('.portletArchiveListing .year span.yearnumber').click(function() {
             $(this).parent().toggleClass('expanded');
-            $(this).parent().children('ul').slideToggle();
         });
     });
 });
